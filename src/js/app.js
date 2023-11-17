d95cc380036d87fa545ddefcf27e74e34a32a25a
elmiramm <elmira.m.m@yandex.ru>
2023-11-17 22:07:38 +0300

    login-form - js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import { isWebp } from "./modules/is-webp.js";
-
+import { form } from "./modules/form.js";
 
 isWebp();
-
+form();
 
---
+++ src/js/modules/form.js
@@ -0,0 +1,34 @@
+export function form() {
+	const form = {
+		form: document.querySelector('.form'),
+		email: document.getElementById('email'),
+		password: document.getElementById('password'),
+		submitBtn: document.getElementById('button')
+	}
+
+	form.email.oninput = (evt) => {
+		const emailInputValue = evt.target.value;
+		if (emailInputValue) {
+			form.email.classList.add('filled');
+		} else {
+			form.email.classList.remove('filled');
+		}
+	}
+
+	form.password.oninput = (evt) => {
+		const emailInputValue = evt.target.value;
+		if (emailInputValue) {
+			form.password.classList.add('filled');
+		} else {
+			form.password.classList.remove('filled');
+		}
+	}
+
+	form.form.addEventListener("submit", () => {
+		if (form.email.value === "mail@m.com" && form.password.value === "password") {
+			alert("Ура, Вы вошли!!!");
+		} else {
+			alert("Неправильно ввеедны email и пароль");
+		}
+	});
+}
